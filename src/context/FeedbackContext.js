a39a9c41c3494cdd32a3ae8d97c40313f1291c6c
Mann Patel <mannpatel2118@gmail.com>
2022-04-28 20:40:46 -0400

    Final Commit

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,42 +1,42 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 /* Context provides a way to pass data through the component tree without having to pass props down manually at every level. */
-import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
     
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: "This is the feedback 1",
-            rating: 9
-        },
-        {
-            id: 2,
-            text: "This is the feedback 2",
-            rating: 10
-        },
-        {
-            id: 3,
-            text: "This is the feedback 3",
-            rating: 8
-        }
-    ])
-
+    const [feedback, setFeedback] = useState([])
+    const [isLoading, setLoading] = useState(true);
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     });
 
-    const addFeedback = (nF) => {
-        nF.id = uuidv4();
-        // The state of the object is immutable - we cant just push onto it. We have to basically make a copy of it.
-        setFeedback([nF, ...feedback])
+    useEffect(() => {
+        fetchFeedback();
+    }, []);
+
+    const fetchFeedback = async () => {
+        const response = await fetch("/feedback?_sort=id&_order=desc");
+        const data = await response.json();
+        setFeedback(data);
+        setLoading(false);
     }
 
-    const deleteFeedback = (id) => {
+
+    const addFeedback = async (nF) => {
+
+        const response = await fetch('/feedback', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(nF) })
+
+        const data = await response.json();
+        // The state of the object is immutable - we cant just push onto it. We have to basically make a copy of it.
+        setFeedback([data, ...feedback])
+    }
+
+    const deleteFeedback = async (id) => {
+        
         if (window.confirm('Are you sure you want to delete ?')) {
+            await fetch(`/feedback/${id}`, { method: 'DELETE' });
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
@@ -48,8 +48,10 @@ export const FeedbackProvider = ({ children }) => {
         })
     }
 
-    const updateFeedback = (id, newItem) => {
-        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...newItem } : item));
+    const updateFeedback = async (id, newItem) => {
+        const response = await fetch(`/feedback/${id}`, { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(newItem) });
+        const data = await response.json();
+        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item));
     }
 
     return <FeedbackContext.Provider value={{
@@ -59,7 +61,8 @@ export const FeedbackProvider = ({ children }) => {
         handleAdd: addFeedback,
         handleEdit: editFeedback,
         feedbackEdit: feedbackEdit,
-        handleUpdate: updateFeedback
+        handleUpdate: updateFeedback,
+        isLoading: isLoading
     }}>
         {children}
     </FeedbackContext.Provider>
